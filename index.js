2e040a69d1d472a0038feb7d291a570665eebe59
pure-matcha-latte <85114612+pure-matcha-latte@users.noreply.github.com>
2021-06-04 11:27:48 +0800

    Exercise 3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = require("./persons");
+let persons = require("./persons");
 
 app.get("/info", (req, res) => {
   res.send(
@@ -20,6 +20,13 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
 });
 
 const PORT = 3001;
